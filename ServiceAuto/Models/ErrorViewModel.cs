c4a37b078854430d8f8bf3ff6e0b236715478170
xaris pap <harispap13@hotmail.gr>
2019-04-10 17:12:16 -0700

    Add project files.

---
+++ ServiceAuto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServiceAuto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
